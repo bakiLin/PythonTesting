37df949825fbc71ef0e086239c06301b3465b729
lin <ilnur_bakiev@inbox.ru>
2024-10-04 10:28:34 +0300

    Added more igredients

--- Task 2/unitest.py
+++ Task 2/unitest.py
@@ -13,6 +13,9 @@ class TestMain(unittest.TestCase):
             "ingredients_list": [
                 ('Яйцо', 100, 50, 100),
                 ('Сахар', 200, 100, 50),
+                ('Соль', 100, 150, 70),
+                ('Eggs', 120, 100, 100),
+                ('Sugar', 300, 140, 90),
             ],
         }
 
@@ -28,16 +31,16 @@ class TestMain(unittest.TestCase):
         print("tearDownClass")
 
     def test_first(self):
-        self.assertEqual(self.recipe_object.calc_cost(), 150)
+        self.assertEqual(self.recipe_object.calc_cost(), 410)
 
     def test_second(self):
-        self.assertEqual(self.recipe_object.calc_cost(2), 300)
+        self.assertEqual(self.recipe_object.calc_cost(2), 820)
 
     def test_third(self):
-        self.assertEqual(self.recipe_object.calc_weight(2), 300)
+        self.assertEqual(self.recipe_object.calc_weight(2), 1080)
 
     def test_fourth(self):
-        self.assertEqual(self.recipe_object.calc_weight(raw=True), 150)
+        self.assertEqual(self.recipe_object.calc_weight(raw=True), 540)
 
 if __name__ == '__main__':
     unittest.main()
